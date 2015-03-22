4a97cdd38311b8825b9b4da2a0068cdb056548ad
Abumova Tatiana <tata.abumova@gmail.com>
2015-03-22 16:53:13 +0300

    Форма седоны для изучения JS

--- js/search-block.js
+++ js/search-block.js
@@ -1,7 +1,26 @@
+/****************БЛОК ПОИСКА***********************/
+
 var btn_search = document.querySelector(".btn--search");
 var search_block = document.querySelector(".search-block");
 
 btn_search.addEventListener("click", function(event) {
 	event.preventDefault();
 	search_block.classList.toggle("search-block--visible");
+});
+
+
+/*******************МЕНЮ*****************************/
+
+var btn_open = document.querySelector(".btn--open");
+var btn_close = document.querySelector(".btn--close");
+var mobile_nav = document.querySelector(".mobile-nav");
+
+btn_open.addEventListener("click", function(event) {
+	event.preventDefault();
+	mobile_nav.classList.toggle("mobile-nav--visible");
+});
+
+btn_close.addEventListener("click", function(event) {
+	event.preventDefault();
+	mobile_nav.classList.remove("mobile-nav--visible");
 });
