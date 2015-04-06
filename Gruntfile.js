8e6ecfe9f721d789ed1c2dbbbd0d2deb7189bd3b
Tata Abumova <tata.abumova@gmail.com>
2015-04-06 23:52:14 +0300

    grunt автоматизация

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+/*module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -29,4 +29,104 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-lintspaces');
 
   grunt.registerTask('lint', ['lintspaces']);
+};*/
+
+
+module.exports = function(grunt) {
+
+ require("load-grunt-tasks")(grunt);
+  /*grunt.loadNpmTasks('grunt-contrib-copy');*/
+
+/*  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-autoprefixer");
+  grunt.loadNpmTasks("grunt-combine-media-queries");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");*/
+
+  grunt.initConfig({
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "img/**",
+            "js/**",
+            "index.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
+    },
+
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": ["source/less/style.less"]
+        }
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ["last 2 version", "ie 10"]
+      },
+      style: {
+        src: "build/css/style.css"
+      }
+    },
+
+    cmq: {
+      style: {
+        files: {
+          "build/css/style.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    cssmin: {
+      style: {
+        options: {
+          keepSpecialComments: 0,
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    csscomb: {
+      style: {
+        expand: true,
+        src: ["less/**/*.less"]
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif,svg"]
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "autoprefixer",
+    "cmq",
+    "cssmin",
+    "csscomb",
+    "imagemin"
+    ]);
 };
